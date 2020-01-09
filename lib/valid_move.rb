de51bdda85fcab214a0399ca0c0e37d5e79adce7
Skye Geraghty <skyetay03@googlemail.com>
2020-01-09 00:32:59 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, pos)
-  return position_taken?(board, pos) ? 
+  return position_taken?(board, pos) ? board[pos] == "X"
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
